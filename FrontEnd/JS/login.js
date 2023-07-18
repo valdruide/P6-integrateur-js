4e673a72db71216abeb85da67cf5e6d4dc7e944c
Tristan Viard <tviardprimo190@gmail.com>
2023-07-19 00:46:55 +0200

    login

---
+++ FrontEnd/JS/login.js
@@ -0,0 +1,42 @@
+const login = {
+      init: function () {
+            const loginBtn = document.getElementById('loginBtn');
+            loginBtn.addEventListener('click', () => {
+                  login.login();
+            });
+      },
+      login: async function () {
+            const mail = document.getElementById('mail').value;
+            const pass = document.getElementById('pass').value;
+            const errorMsg = document.querySelector('.errorMsg');
+            errorMsg.style.visibility = 'hidden';
+            errorMsg.classList.remove('errorAnim');
+            const user = {
+                  email: mail,
+                  password: pass,
+            };
+            try {
+                  const result = await fetch('http://localhost:5678/api/users/login', {
+                        method: 'POST',
+                        headers: {
+                              'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(user),
+                  });
+                  const res = await result.json();
+                  console.log(res);
+                  if (res.message || res.error) {
+                        errorMsg.style.visibility = 'visible';
+                        errorMsg.classList.add('errorAnim');
+                  } else {
+                        const token = res.token;
+                        localStorage.setItem('token', token);
+                        window.location.href = 'index.html';
+                  }
+            } catch (err) {
+                  console.error(err);
+            }
+      },
+};
+
+document.addEventListener('DOMContentLoaded', login.init);
--- FrontEnd/JS/main.js
+++ FrontEnd/JS/main.js
@@ -1,66 +1,84 @@
 const indexPage = {
-  init: function () {
-    indexPage.getCat();
-    indexPage.getWorks();
-  },
-  getCat: async function () {
-    try {
-      const response = await fetch("http://localhost:5678/api/categories");
-      const res = await response.json();
-      indexPage.displayCat(res)
-      
-    } catch (err) {
-      console.error(err);
-    }
-  },
-  displayCat: function (res) {
-    const filterContainer = document.getElementById('filterContainer')
+      init: function () {
+            indexPage.getCat();
+            indexPage.getWorks();
+      },
 
-      for(cat of res){
-        const filter = document.createElement('div')
-        filter.classList.add('filter')
-        filter.setAttribute('id', cat.id)
-        const filterTxt = document.createElement('p')
-        filterTxt.innerText = cat.name
-        filter.appendChild(filterTxt)
-        filterContainer.appendChild(filter)
-      }
-      const filter = document.querySelectorAll('.filter');
-      let previousFilter = filter[0];
-      for(let i = 0; i < filter.length; i++){
-        filter[i].addEventListener('click', () => {
-            previousFilter.classList.remove('activeFilter')
-            previousFilter = filter[i];
-            filter[i].classList.add('activeFilter');
-            console.log('cliock');
-        })
-      }
-  },
-  getWorks: async function(){
-    try{
-        const response = await fetch("http://localhost:5678/api/works");
-        const works = await response.json();
-        indexPage.displayWorks(works)
-    } catch (err){
-        console.error(err);
-    }
-  },
-  displayWorks: function (works){
-    const worksContainer = document.getElementById('gallery');
-
-    for(work of works){
-        const figure = document.createElement('figure');
-        figure.setAttribute('categoryId', work.categoryId);
-        const img = document.createElement('img');
-        img.setAttribute('src', work.imageUrl);
-        img.setAttribute('alt', work.title);
-        const figcaption = document.createElement('figcaption');
-        figcaption.innerText = work.title;
-        figure.appendChild(img);
-        figure.appendChild(figcaption);
-        worksContainer.appendChild(figure);
-    }
-  }
+      getCat: async function () {
+            //Récupère toutes les catégories
+            try {
+                  const response = await fetch('http://localhost:5678/api/categories');
+                  const res = await response.json();
+                  indexPage.displayCat(res); //Lance la fonction displayCat avec en paramètre les résultats du fetch
+            } catch (err) {
+                  console.error(err);
+            }
+      },
+      displayCat: function (res) {
+            //Affiche les filtres
+            const filterContainer = document.getElementById('filterContainer');
+            for (cat of res) {
+                  const filter = document.createElement('div');
+                  filter.classList.add('filter');
+                  filter.setAttribute('id', cat.id);
+                  const filterTxt = document.createElement('p');
+                  filterTxt.innerText = cat.name;
+                  filter.appendChild(filterTxt);
+                  filterContainer.appendChild(filter);
+            }
+            //ajoute un click event sur chaque flitres
+            const filter = document.querySelectorAll('.filter');
+            for (let i = 0; i < filter.length; i++) {
+                  filter[i].addEventListener('click', () => {
+                        //Supprime la classe activeFilter de tous les filtres avant de le mettre sur celui cliqué
+                        filter.forEach((filter) => {
+                              filter.classList.remove('activeFilter');
+                        });
+                        indexPage.filterCat(filter[i]);
+                  });
+            }
+      },
+      filterCat: function (filter) {
+            //Ajoute la classe activeFilter sur le filtre cliqué
+            filter.classList.add('activeFilter');
+            //Affiche ou masque les travaux en fonction du filtre
+            let catId = filter.getAttribute('id');
+            let allWorks = document.querySelectorAll('.workFigure');
+            for (let i = 0; i < allWorks.length; i++) {
+                  if (catId !== allWorks[i].getAttribute('categoryId') && catId !== '0') {
+                        allWorks[i].style.display = 'none';
+                  } else {
+                        allWorks[i].style.display = 'block';
+                  }
+            }
+      },
+      getWorks: async function () {
+            //récupère tous les travaux
+            try {
+                  const response = await fetch('http://localhost:5678/api/works');
+                  const works = await response.json();
+                  indexPage.displayWorks(works);
+            } catch (err) {
+                  console.error(err);
+            }
+      },
+      displayWorks: function (works) {
+            //Affiche tous les travaux
+            const worksContainer = document.getElementById('gallery');
+            for (work of works) {
+                  const figure = document.createElement('figure');
+                  figure.setAttribute('categoryId', work.categoryId);
+                  figure.classList.add('workFigure');
+                  const img = document.createElement('img');
+                  img.setAttribute('src', work.imageUrl);
+                  img.setAttribute('alt', work.title);
+                  const figcaption = document.createElement('figcaption');
+                  figcaption.innerText = work.title;
+                  figure.appendChild(img);
+                  figure.appendChild(figcaption);
+                  worksContainer.appendChild(figure);
+            }
+      },
 };
 
-document.addEventListener("DOMContentLoaded", indexPage.init);
+document.addEventListener('DOMContentLoaded', indexPage.init);
